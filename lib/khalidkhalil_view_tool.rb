2aad41ea3bd6ff2040727368f35f76c6918e04c8
khalidkhalil1993 <kfk@webshipper.com>
2018-09-11 14:06:25 +0200

    initial commit

---
+++ lib/khalidkhalil_view_tool.rb
@@ -0,0 +1,6 @@
+require "khalidkhalil_view_tool/version"
+require "khalidkhalil_view_tool/renderer"
+
+module KhalidkhalilViewTool
+  
+end
---
+++ lib/khalidkhalil_view_tool/version.rb
@@ -0,0 +1,3 @@
+module KhalidkhalilViewTool
+  VERSION = "0.1.0"
+end
